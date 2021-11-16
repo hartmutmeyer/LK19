0ac507c471f97bf49ce9756315ece25dd4306294
Hartmut Meyer <h.meyer6@schule.bremen.de>
2021-11-16 11:39:17 +0100

    .

--- 38_JavaSQL_Wiederholung/loesungen/ExklusivReisen.java
+++ 38_JavaSQL_Wiederholung/loesungen/ExklusivReisen.java
@@ -1,4 +1,5 @@
 import java.awt.Dimension;
+import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.Connection;
@@ -15,6 +16,9 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
+
+import tmpQ2.ExklusivReisen;
+
 import javax.swing.ListSelectionModel;
 import javax.swing.JScrollPane;
 
@@ -284,6 +288,15 @@ public class ExklusivReisen extends JFrame {
 	// Ende Ereignisprozeduren
 
 	public static void main(String[] args) {
-		new ExklusivReisen();
+		EventQueue.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					new ExklusivReisen();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 }
